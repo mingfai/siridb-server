c41082cf8293da32265f201c5157a90d6e2f55bb
fai <fai@bot.hk>
2020-03-25 18:13:18 +0800

    disabled user access checking

--- src/siri/net/clserver.c
+++ src/siri/net/clserver.c
@@ -522,7 +522,7 @@ static void on_insert(sirinet_stream_t * client, sirinet_pkg_t * pkg)
                 SIRIDB_ACCESS_INSERT,
                 err_msg))
     {
-        log_warning("(%s) %s",
+        log_warn("(%s) %s",
                 sirinet_cproto_server_str(CPROTO_ERR_USER_ACCESS),
                 err_msg);
         sirinet_pkg_t * package = sirinet_pkg_err(
